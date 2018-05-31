8904ecfb844271a2e58108988a481c52054103d3
Anoop <anoop.a.goudar@gmail.com>
2018-05-31 16:12:24 +0530

    added problem statement

--- sortMillionNumbers.js
+++ sortMillionNumbers.js
@@ -1,3 +1,8 @@
+// Exercise 2
+
+// We have a file which has 10 million numbers put in randomly ranging from 0 to 99.
+// Sort the array and write it to another file without keeping the whole array into memory or reading the whole file at once and putting it in memory. 
+
 var fs = require('fs');
 
 function generateRandomNumbers(from, to, totalRandomNumbers) {
